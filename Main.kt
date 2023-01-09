4e1d06ef23113b8122a83b59a5c47c1211ec20d1
Mateusz Galaś <mateuszgalas94@gmail.com>
2023-01-09 11:58:30 +0100

    Changed function hide, show, added functions changeToBinary, encryptMessage, decryptMessage.

--- Main.kt
+++ Main.kt
@@ -5,15 +5,41 @@ import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
 
+// Encrypting message with password using operator xor
+fun encryptMessage(): String {
+    println("Message to hide:")
+    val message = changeToBinary(readln())
+    println("Password:")
+    val password = changeToBinary(readln())
+
+    var encryptedMessage = ""
+    var counter = 0
+    // Encrypting
+    message.forEach {
+        encryptedMessage += it.digitToInt() xor password[counter].digitToInt()
+        if (counter == password.length - 1) counter = 0 else counter++
+    }
+
+    // Adding stop value to the end of encrypted message
+    return encryptedMessage + "000000000000000000000011"
+}
+
+// Changing format from String text to string '00110101...'
+fun changeToBinary(text: String): String {
+    return text.encodeToByteArray().joinToString("") {
+        String.format("%08d", it.toString(2).toInt())
+    }
+}
+
+
+// Hide encrypted message into image
 fun hide() {
     println("Input image file:")
     val inputImage = File(readln())
     println("Output image file:")
     val outputImage = File(readln())
-    println("Message to hide:")
-    val message = readln().encodeToByteArray().plus(listOf(0, 0, 3)).joinToString("") {
-        String.format("%08d", it.toString(2).toInt())
-    }
+
+    val message = encryptMessage()
 
     if (!inputImage.exists()) println("Can't read input file!").also { return }
     if (!outputImage.exists()) outputImage.createNewFile()
@@ -21,21 +47,20 @@ fun hide() {
     val image = ImageIO.read(inputImage)
     val myOutput = BufferedImage(image.width, image.height, BufferedImage.TYPE_INT_RGB)
 
-    if (message.length * 8 > image.width * image.height) {
+    if ((message.length + 3) * 8 > image.width * image.height) {
         println("The input image is not large enough to hold this message.")
         return
     }
-    //println(message)
     var counter = 0
-    // Change the least significant bit for red, green and blue to 1
-    for (x in 0 until image.width) {
-        for (y in 0 until image.height) {
+    // Change the least significant bit for blue with message bits and save it to image
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
             val i = Color(image.getRGB(x, y))
 
             if (counter < message.length) {
                 val color = Color(
                     i.red, i.green,
-                    if (i.blue.takeLowestOneBit() == 1) i.blue and message[counter].digitToInt()
+                    if (i.blue.takeLowestOneBit() == 1) i.blue + if (message[counter].digitToInt() == 1) 0 else -1
                     else i.blue or message[counter].digitToInt()
                 )
                 myOutput.setRGB(x, y, color.rgb)
@@ -46,37 +71,49 @@ fun hide() {
         }
     }
     ImageIO.write(myOutput, "png", outputImage)
-
-    // Replace to pass the tests
-    /*println(
-        "Input Image: ${inputImage.toString().replace("\\", "/")}\n" +
-                "Output Image: ${outputImage.toString().replace("\\", "/")}"
-    )*/
     println("Message saved in ${outputImage.toString().replace("\\", "/")} image.")
 }
 
+fun decryptMessage(encryptedMessage: String): String {
+    println("Password:")
+    val password = changeToBinary(readln())
+    var message = ""
+    var counter = 0
+    // Decrypting message using password na operator xor
+    encryptedMessage.forEach {
+        if (it != ' ') {
+            message += it.digitToInt() xor password[counter].digitToInt()
+            if (counter == password.length - 1) counter = 0 else counter++
+        } else {
+            message += it
+        }
+    }
+    // Change from binary to characters form
+    message = message.split(" ")
+        .filter { it != "" }
+        .map { it.toInt(2).toChar() }
+        .joinToString("")
+    return message
+}
 fun show() {
     println("Input image file:")
     val imageFile = File(readln())
     val image = ImageIO.read(imageFile)
+
     var text = ""
-    var message = ""
-    for (x in 0 until image.width) {
-        for (y in 0 until image.height) {
+    var encryptedMessage = ""
+    for (y in 0 until image.height) {
+        for (x in 0 until image.width) {
             val i = Color(image.getRGB(x, y))
             text += if (i.blue.takeLowestOneBit() == 1) 1 else 0
 
             if (text.length == 8) {
-                message += "${text.toInt()} "
+                encryptedMessage += "$text "
                 text = ""
             }
-            if (message.contains("0 0 11")) {
-                message = message.replace("0 0 11", "")
-                message = message.split(" ")
-                    .filter { it != "" }
-                    .map { it.toInt(2).toChar() }
-                    .joinToString("")
-                println("Message:\n$message")
+            if (encryptedMessage.contains("00000000 00000000 00000011")) {
+                encryptedMessage = encryptedMessage.replace("00000000 00000000 00000011", "")
+                println("Message:\n${decryptMessage(encryptedMessage)}")
                 return
             }
         }
@@ -95,4 +132,3 @@ fun main() {
     }
     println("Bye!")
 }
-
